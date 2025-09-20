0da832d645f1067be884130374acbdb75ca7698e
Pedro Almeida <ip4dro@gmail.com>
2025-09-20 03:14:58 +0200

    manually update swift dependencies from 6.1 to 6.2.1

--- Package.swift
+++ Package.swift
@@ -942,7 +942,7 @@ func swiftSyntaxDependencies(_ names: [String]) -> [Target.Dependency] {
 // this right now.
 
 /// When not using local dependencies, the branch to use for llbuild and TSC repositories.
-let relatedDependenciesBranch = "release/6.1"
+let relatedDependenciesBranch = "release/6.2.1"
 
 if ProcessInfo.processInfo.environment["SWIFTPM_LLBUILD_FWK"] == nil {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
